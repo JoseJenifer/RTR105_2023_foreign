471488f569a6cbf30abcbd9a7c68781b1fbe3566
JoseJenifer <144117879+JoseJenifer@users.noreply.github.com>
2023-11-20 20:37:20 +0200

    Programming Task 4

---
+++ PT4.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+double num;
+printf("Enter decimal number:\n");
+scanf("%lf",&num);
+
+char datatype;
+printf("Enter a data type (c for char, i for int, l for lon long):\n");
+scanf("%c", &datatype);
+
+long long factorial =1;
+if (datatype=='l'||datatype=='i'&& num<0)
+{
+printf("The calculation is not possible for negative numbers for slected datatypes: \n");
+return 1;
+}
+while(num>0)
+{
+if(datatype=='i'&& factorial>2147483647/num)
+{printf("Calculation is not possible for given datatype \n");
+return 1;}
+
+if(datatype=='l'&&factorial>9223372036854775807/num)
+{printf("calculation is not possilble for the given datatype \n");
+return 1;}
+
+if(datatype=='c'&&factorial>255/num) 
+{printf("Calculation is not possible for the given datatype \n");
+return 1;}
+
+
+factorial *=(long long) num;
+num--;
+}
+printf("factorial %lld: \n", factorial);
+return 0;
+}

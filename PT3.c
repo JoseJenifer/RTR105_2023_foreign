199a7bbf47f31d4f65975ff32be37a0298173109
JoseJenifer <144117879+JoseJenifer@users.noreply.github.com>
2023-11-30 18:40:57 +0200

    Programming Task 3

--- PT3.c
+++ PT3.c
@@ -1,85 +1,81 @@
 #include<stdio.h>
 void sortNumbers(int a, int b, int c, int order)
 {
+ int temp;
+  if (order==1)
+  {
+    if (a>=b)
+    {
+      temp=a;
+      a=b;
+      b=temp;
+    }
 
+    if(b>=c)
+    {
+      temp=b;
+      b=c;
+      c=temp;
+    }
 
-
-int temp;
-
-if (order==1)
-{
-if (a>b)
-{
-temp=a;
-a=b;
-b=temp;
-}
-
-if(b>c)
-{
-temp=b;
-b=c;
-c=temp;
-}
-
-if(a>b)
-{
-temp=a;
-a=b;
-b=temp;
-}
+    if(a>=b)
+    {
+      temp=a;
+      a=b;
+      b=temp;
+    }
 
 
 }
 
-else if(order==2)
-{
-if(a<b)
-{
-temp=a;
-a=b;
-b=temp;
+  else if(order==2)
+  {
+    if(a<=b)
+    {
+      temp=a;
+      a=b;
+      b=temp;
+    }
+
+    if(b<=c)
+    {
+      b=temp;
+      b=c;
+      c=temp;
+    }
+
+    if(a<=b)
+    {
+      temp=a;
+      a=b;
+      b=temp;
+    }
+
+
 }
 
-if(b<c)
-{
-b=temp;
-b=c;
-c=temp;
-}
-
-if(a<b)
-{
-temp=a;
-a=b;
-b=temp;
-}
-
-
-}
-
-else
-{
-printf("Invalid output order. Please type 1 for ascending order and 2 for descending order. \n");
-return ;
-}
-printf(" Sorted sequence of numbers is: %d, %d, %d \n",a,b,c);
-}
+    else
+    {
+       printf("Invalid output order. Please type 1 for ascending order and 2 for descending order. \n");
+       return ;
+    }
+       printf(" Sorted sequence of numbers is: %d, %d, %d \n",a,b,c);
+}    
 
 int main()
 {
-int a,b,c,order;
-printf("Enter first decimal number:\n");
-scanf("%d",&a);
-printf("Enter second decimal number:\n");
-scanf("%d",&b);
-printf("Enter third decimal number:\n");
-scanf("%d",&c);
+   int a,b,c,order;
+   printf("Enter first decimal number:\n");
+   scanf("%d",&a);
+   printf("Enter second decimal number:\n");
+   scanf("%d",&b);
+   printf("Enter third decimal number:\n");
+   scanf("%d",&c);
 
-printf("Enter the order of sequence (1 for ascending or 2 for descending):\n");
-scanf("%d",&order);
+   printf("Enter the order of sequence (1 for ascending or 2 for descending):\n");
+   scanf("%d",&order);
 
-sortNumbers(a,b,c,order);
+   sortNumbers(a,b,c,order);
 
 return 0;
 }

afb04cf5a6f62b0de93be24e5d7433961d8694ec
JoseJenifer <144117879+JoseJenifer@users.noreply.github.com>
2024-01-04 18:34:42 +0200

    Programming Task 4

--- PT4.c
+++ PT4.c
@@ -1,38 +1,35 @@
 #include<stdio.h>
 int main()
 {
-  int num;
+  int  num;
   char datatype;
-  
-  printf("Enter a decimal number: \n");
-  scanf("%d",&num);
- 
-  while(getchar()!='\n');
 
-  printf("Enter datatype(c for char,i for int, or ll for long long): \n");
+  printf("Enter a decimal number:");
+  scanf("%d", &num);
+  
+  while(getchar() != '\n');
+  
+  printf("Enter a datatype(c for char,i for int or ll for long long):\n");
   scanf("%c",&datatype);
 
   long long result = 1;
-  int i = 1;
+  int i =1;
 
   long long temp = 0;
 
-  while(i <= num)
+  while (i<= num)
   {
-    temp = result*i;
-     
-    if(temp/i != result)
+    temp=result*i;
+    if(temp/i!=result)
     {
-      printf("Error:Overflow for the given datatype.\n");
+      printf("error overflow for given datatype.\n");
       return 0;
     }
-   
-    result = temp;
-    i++;
+    
+   result = temp;
+   i++;
   }
-
-  printf("Factorial pf %d with the given datatype is : &lld \n", num, result);
   
+  printf("factorial of %d with datatype %c: %lld\n",num, datatype, result);
   return 0;
-
 }

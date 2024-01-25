6ac6c98f0ce78d0c1710703732c3d5e6a9320fa4
JoseJenifer <144117879+JoseJenifer@users.noreply.github.com>
2024-01-25 15:53:38 +0200

    Programming task 4

--- PT4.c
+++ PT4.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-int main()
+void main()
 {
-  int  num;
+  long long num;
+  char x = 1;
+  int y = 1;
+  long long z = 1;
   char datatype;
 
   printf("Enter a decimal number:");
@@ -9,27 +12,76 @@ int main()
   
   while(getchar() != '\n');
   
-  printf("Enter a datatype(c for char,i for int or ll for long long):\n");
+  printf("Enter a datatype(1 for char,2 for int or 3 for long long):\n");
   scanf("%c",&datatype);
 
-  long long result = 1;
-  int i =1;
-
-  long long temp = 0;
-
-  while (i<= num)
+  
+  if(datatype == '1')
   {
-    temp=result*i;
-    if(temp/i!=result)
+    char factorial = 1;
+    int a = 1;
+    
+    while(a < num)
     {
-      printf("Error.Overflow for given datatype.\n");
-      return 0;
+       x *= (a+1);
+       factorial *= a;
+       a++;
     }
     
-   result = temp;
-   i++;
+    if(x != factorial*num)
+    {
+       printf("Error. Overflow detected.\n");
+    }
+    
+    else if( x == factorial*num)
+    {
+       printf("Factorial of the given number is: %d. \n",x);
+    }
   }
   
-  printf("Factorial of %d with datatype : %lld \n",num, result);
-  return 0;
-}
+  if(datatype == '2')
+  {
+    int factorial = 1;
+    int a = 1;
+    
+    while(a < num)
+    {
+      y *= (a+1);
+      factorial *= a;
+      a++;
+    }
+    
+    if(y != factorial*num)
+    {
+       printf("Error. Overflow Detected. \n");
+    }
+    
+    else if(y == factorial*num)
+    {
+       printf("Factorial of the given number is: %d. \n",y);    
+    {
+  }
+  
+  if(datatype == '3')
+  {
+    long long factorial = 1;
+    long long a = 1;
+    
+    while(a < num)
+    {
+      z *= (a+1);
+      factorial *= a;
+      a++;
+    }
+    
+    if(z != factorial*num)
+    {
+      printf("Error.Overflow detected. \n");
+    }
+    
+    else if(z == factorial*num)
+    {
+      printf("Factorial of the given number is: %d. \n", z);
+    }
+  }   
+}    

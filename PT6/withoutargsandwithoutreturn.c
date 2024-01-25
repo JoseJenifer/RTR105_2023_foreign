1486ae702d42eb3c6fed95ac0481a5683f3c0260
JoseJenifer <144117879+JoseJenifer@users.noreply.github.com>
2024-01-25 20:55:06 +0200

    withoutargsandwithoutreturn.c

--- PT6/withoutargsandwithoutreturn.c
+++ PT6/withoutargsandwithoutreturn.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 void printMessage()
 {
-printf("The best way to predict future is to invent it.\n");
+  printf("The best way to predict future is to invent it.\n");
+}
+
+int main()
+{
+  printMessage();
+  return 0;
 }
